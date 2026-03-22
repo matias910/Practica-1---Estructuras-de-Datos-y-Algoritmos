bd0dbd5c882db328d58e19279fc72f3c855576c2
Matias Zapata Rojas <matiaszptro@gmail.com>
2026-03-22 13:42:55 -0500

    QuickSort agregado

--- Practica/include/quicksort.h
+++ Practica/include/quicksort.h
@@ -4,10 +4,11 @@
 
 #ifndef PRACTICA_1_ESTRUCTURAS_DE_DATOS_Y_ALGORITMOS_QUICKSORT_H
 #define PRACTICA_1_ESTRUCTURAS_DE_DATOS_Y_ALGORITMOS_QUICKSORT_H
+#pragma once
+#include <vector>
+#include <string>
 
-
-class quicksort {
-};
-
+void quickSort(std::vector<std::string>& arr, int low, int high);
+int  partition(std::vector<std::string>& arr, int low, int high);
 
 #endif //PRACTICA_1_ESTRUCTURAS_DE_DATOS_Y_ALGORITMOS_QUICKSORT_H
